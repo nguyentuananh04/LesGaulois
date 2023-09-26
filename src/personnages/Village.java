4a89da03e608a97641c5235c2db1912184dfd15e
nguyentuananh04 <tuan-anh.nguyen@univ-tlse3.fr>
2023-09-26 19:59:47 +0200

    TP2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,10 +3,16 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
 	
-	public Village(String nom, Chef chef) {
+	public Village(String nom, Chef chef, int nbVillageoisMaximum) {
 		this.nom = nom;
 		this.chef = chef;
+		this.nbVillageois = nbVillageois;
+		if (nbVillageois < nbVillageoisMaximum) {
+			villageois[nbVillageois] 
+		}
 	}
 	
 	public String getNom() {
